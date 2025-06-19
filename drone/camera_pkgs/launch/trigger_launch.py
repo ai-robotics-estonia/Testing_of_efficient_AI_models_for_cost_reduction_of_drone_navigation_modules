b3305e4b18ccf33094ef2c7f1881446a3561f77f
anish <anish.shrestha@ut.ee>
2025-06-19 11:56:15 +0300

    removed comments

--- drone/camera_pkgs/launch/trigger_launch.py
+++ drone/camera_pkgs/launch/trigger_launch.py
@@ -4,9 +4,7 @@ from launch.actions import IncludeLaunchDescription
 import launch.launch_description_sources
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
-from launch.actions import TimerAction, ExecuteProcess
 import os
-import time
 
 
 def generate_launch_description():
@@ -30,15 +28,7 @@ def generate_launch_description():
             name='trigger_node',
             output='screen',
             parameters=[]
-        )
-
-    # gps_node = Node(
-            # package='camera_pkgs',
-            # executable='publish_gps',
-            # name='publish_gps',
-            # output='screen',
-            # parameters=[]
-        # )    
+        )   
 
     urdf_file = 'box.urdf'
     urdf_path = os.path.join(camera_package_dir, 'urdf', urdf_file)
@@ -53,26 +43,8 @@ def generate_launch_description():
             output='screen',
             parameters=[{'robot_description': robot_description}]
         )
-
-    # bags_directory = "/home/rpi5/ros2_jazzy/src/drone/camera_pkgs/bags/"
-    # os.makedirs(bags_directory, exist_ok=True)
-
-    # bag_record = ExecuteProcess(
-            # cmd=['ros2', 'bag', 'record', '-o', bags_directory+str(time.time()), 
-                 # '--topics', '/barometer', 
-                             # '/cam/front/image', 
-                             # '/cam/bottom/image', 
-                             # '/imu', 
-                             # '/lidar', 
-                             # '/magnetic_field',
-                             # '/tf',
-                             # '/tf_static',
-                             # '/gps',
-                             # '/heading'],
-                # output='screen'
-        # )   
-
     
+
     return LaunchDescription([        
         
         IncludeLaunchDescription(
@@ -81,11 +53,6 @@ def generate_launch_description():
         
         box_urdf_publisher,
         distance_node,
-        trigger_node,
-        #gps_node,       
-
-        # TimerAction(period=10.0,
-                    # actions=[bag_record],
-                    # ),               
+        trigger_node,               
 
     ])
